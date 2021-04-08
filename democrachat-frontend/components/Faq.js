06da3a94f9279d1bc58fb061348dacdc130b72c4
malpas <40313754+malpas@users.noreply.github.com>
2021-04-08 15:03:39 +1000

    Update FAQ with usage of silver

--- democrachat-frontend/components/Faq.js
+++ democrachat-frontend/components/Faq.js
@@ -10,17 +10,20 @@ function Faq() {
         <ul>
             <li><a href="#whatisdemocrachat">What is Democrachat?</a></li>
             <li><a href="#whatdoes00mean">What does 0G 0S mean?</a></li>
+            <li><a href="#useofsilver">What can I do with silver?</a></li>
             <li><a href="#getgold">How do I get gold?</a></li>
             <li><a href="#technicalissues">I'm having a technical issue. Help!</a></li>
             <li><a href="#topicbidding">What is topic bidding?</a></li>
-            <li><a href="#real">Are these real questions?</a></li>
         </ul>
 
-        <h3 id="whatisdemocrachat">What is Democrachat</h3>
+        <h3 id="whatisdemocrachat">What is Democrachat?</h3>
         <p>An experimental chat site about community-driven moderation.</p>
 
         <h3 id="whatdoes00mean">What does 0G 0S mean?</h3>
         <p>"0G" means you have 0 gold, while "0S" means you have zero silver. Gold generates over time, while silver can be used for goodies such as muting bad people and bidding on new topics.</p>
+
+        <h3 id="useofsilver">What can I do with silver?</h3>
+        <p>Silver is mostly used for muting people and topic bidding.</p>
 
         <h3 id="getgold">How do I get gold?</h3>
         <p>Be patient and keep contributing to conversations. There are currently no standardized ways to earn gold.</p>
@@ -30,9 +33,6 @@ function Faq() {
 
         <h3 id="topicbidding">What is topic bidding?</h3>
         <p>Topic bidding allows users to spending gold to suggest new topic rooms. At a certain threshold of silver, topics are automatically considered for addition to the Democrachat universe.</p>
-
-        <h3 id="real">Are these real questions?</h3>
-        <p>Good question.</p>
     </div>
 }
 
