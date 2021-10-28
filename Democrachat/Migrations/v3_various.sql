6c7980a8e12f1c811be8ea0d3f1aec8b2c5d05b5
malpas <40313754+malpas@users.noreply.github.com>
2021-10-29 02:42:02 +1000

    Add default topics to simplify database setup

--- Democrachat/Migrations/v3_various.sql
+++ Democrachat/Migrations/v3_various.sql
@@ -2,3 +2,7 @@
 
 --changeset creator:add-login-ip
 ALTER TABLE login ADD COLUMN ip cidr;
+
+--changeset creator:add-default-topics
+INSERT INTO topic (name) VALUES ('general'), ('music'), ('gaming')
+ON CONFLICT DO NOTHING;
